f9870be9fbf5f9dcd298926b23f93e011c30b18b
Vo Van Nghia <vanvnghia@gmail.com>
2022-11-21 18:00:54 +0100

    cmake: fix configure error on second run

--- cmake/external/libigl.cmake
+++ cmake/external/libigl.cmake
@@ -2,11 +2,12 @@ if(TARGET igl::core)
   return()
 endif()
 
+set(LIBIGL_GIT_TAG v2.4.0)
 include(FetchContent)
 FetchContent_Declare(
   libigl
   GIT_REPOSITORY https://github.com/libigl/libigl.git
-  GIT_TAG v2.4.0
-  PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different "${PROJECT_ROOT_DIR}/cmake/external/libigl.patch" <SOURCE_DIR> && git apply <SOURCE_DIR>/libigl.patch
+  GIT_TAG "${LIBIGL_GIT_TAG}"
+  PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different "${PROJECT_ROOT_DIR}/cmake/external/libigl.patch" <SOURCE_DIR> && git reset --hard "${LIBIGL_GIT_TAG}" && git apply <SOURCE_DIR>/libigl.patch
 )
 FetchContent_MakeAvailable(libigl)
