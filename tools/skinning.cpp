94e2fc6415b491f0c32a57fef0d57ecfa29912a6
Vo Van Nghia <vanvnghia@gmail.com>
2022-11-21 12:50:06 +0100

    tools(skinning): use cmd parser

--- tools/skinning.cpp
+++ tools/skinning.cpp
@@ -5,21 +5,38 @@
 #include <igl/readTGF.h>
 
 #include <Eigen/Geometry>
+#include <cxxopts.hpp>
+#include <iostream>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
+  cxxopts::Options options("skinning", "Skinning on deformed mesh");
+  auto options_adder = options.add_options();
+  options_adder("help", "Print help");
+  options_adder("m,mesh", "Mesh path (.ply)", cxxopts::value<std::string>());
+  options_adder("t,texture", "Texture path (.png)",
+                cxxopts::value<std::string>());
+  options_adder("s,skeleton", "Skeleton path (.tgf)",
+                cxxopts::value<std::string>());
+  auto args = options.parse(argc, argv);
+
+  if (args.count("help")) {
+    std::cout << options.help() << std::endl;
+    return EXIT_SUCCESS;
+  }
+
   Eigen::MatrixXd V, N, UV;
   Eigen::MatrixXi F, E;
-  igl::readPLY(argv[1], V, F, E, N, UV);
+  igl::readPLY(args["mesh"].as<std::string>(), V, F, E, N, UV);
 
   Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic> R;
   Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic> G;
   Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic> B;
   Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic> A;
-  igl::png::readPNG(argv[2], R, G, B, A);
+  igl::png::readPNG(args["texture"].as<std::string>(), R, G, B, A);
 
   Eigen::MatrixXd C;
   Eigen::MatrixXi BE;
-  igl::readTGF(argv[3], C, BE);
+  igl::readTGF(args["skeleton"].as<std::string>(), C, BE);
 
   Eigen::VectorXi P;
   igl::directed_edge_parents(BE, P);
