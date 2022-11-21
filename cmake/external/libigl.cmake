023e6a32e39d1db1ebd139c5e3ee786d736586f3
Vo Van Nghia <vanvnghia@gmail.com>
2022-11-21 17:38:08 +0100

    libigl: apply headless render patch

--- cmake/external/libigl.cmake
+++ cmake/external/libigl.cmake
@@ -7,5 +7,6 @@ FetchContent_Declare(
   libigl
   GIT_REPOSITORY https://github.com/libigl/libigl.git
   GIT_TAG v2.4.0
+  PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different "${PROJECT_ROOT_DIR}/cmake/external/libigl.patch" <SOURCE_DIR> && git apply <SOURCE_DIR>/libigl.patch
 )
 FetchContent_MakeAvailable(libigl)
