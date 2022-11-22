1f15e7b6a0dd115d3a270acdb755e8abe76b3657
Vo Van Nghia <vanvnghia@gmail.com>
2022-11-22 17:51:49 +0100

    tools(skinning): load precomputed weight

--- tools/skinning.cpp
+++ tools/skinning.cpp
@@ -1,21 +1,11 @@
-#include <igl/barycenter.h>
-#include <igl/bbw.h>
-#include <igl/boundary_conditions.h>
-#include <igl/boundary_facets.h>
-#include <igl/copyleft/cgal/remesh_self_intersections.h>
-#include <igl/copyleft/tetgen/mesh_with_skeleton.h>
-#include <igl/copyleft/tetgen/tetrahedralize.h>
 #include <igl/directed_edge_parents.h>
 #include <igl/lbs_matrix.h>
-#include <igl/normalize_row_sums.h>
 #include <igl/opengl/glfw/Viewer.h>
 #include <igl/png/readPNG.h>
 #include <igl/png/writePNG.h>
+#include <igl/readDMAT.h>
 #include <igl/readPLY.h>
 #include <igl/readTGF.h>
-#include <igl/remove_unreferenced.h>
-#include <igl/volume.h>
-#include <igl/winding_number.h>
 
 #include <Eigen/Geometry>
 #include <algorithm>
@@ -52,80 +42,6 @@ void compute_transformation_matrix(const Eigen::MatrixXd& skeleton,
   }
 }
 
-bool clean_mesh(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F,
-                Eigen::MatrixXd& CV, Eigen::MatrixXi& CF) {
-  {
-    Eigen::MatrixXi _1;
-    Eigen::VectorXi _2, IM;
-    igl::copyleft::cgal::remesh_self_intersections(V, F, {}, CV, CF, _1, _2,
-                                                   IM);
-    std::for_each(CF.data(), CF.data() + CF.size(),
-                  [&IM](int& a) { a = IM(a); });
-    Eigen::MatrixXd oldCV = CV;
-    Eigen::MatrixXi oldCF = CF;
-    igl::remove_unreferenced(oldCV, oldCF, CV, CF, IM);
-  }
-  Eigen::MatrixXd TV;
-  Eigen::MatrixXi TT;
-  {
-    Eigen::MatrixXi _1;
-    if (igl::copyleft::tetgen::tetrahedralize(CV, CF, "", TV, TT, _1) != 0) {
-      return false;
-    }
-  }
-  {
-    Eigen::MatrixXd BC;
-    igl::barycenter(TV, TT, BC);
-    Eigen::VectorXd W;
-    igl::winding_number(V, F, BC, W);
-    W = W.array().abs();
-    const double thresh = 0.5;
-    const int count = (W.array() > thresh).cast<int>().sum();
-    Eigen::MatrixXi CT(count, TT.cols());
-    int c = 0;
-    for (int t = 0; t < TT.rows(); t++) {
-      if (W(t) > thresh) {
-        CT.row(c++) = TT.row(t);
-      }
-    }
-    igl::boundary_facets(CT, CF);
-  }
-  return true;
-}
-
-bool compute_tet_mesh(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F,
-                      const Eigen::MatrixXd& C, const Eigen::MatrixXi& BE,
-                      Eigen::MatrixXd& TV, Eigen::MatrixXi& TT,
-                      Eigen::MatrixXi& TF) {
-  Eigen::MatrixXd CV;
-  Eigen::MatrixXi CF;
-  if (!clean_mesh(V, F, CV, CF)) {
-    return false;
-  }
-  return igl::copyleft::tetgen::mesh_with_skeleton(CV, CF, C, {}, BE, {}, 10,
-                                                   "", TV, TT, TF);
-}
-
-bool compute_bbw(const Eigen::MatrixXd& V, const Eigen::MatrixXd& TV,
-                 const Eigen::MatrixXi& TT, const Eigen::MatrixXd& C,
-                 const Eigen::MatrixXi& BE, Eigen::MatrixXd& W) {
-  Eigen::VectorXi b;
-  Eigen::MatrixXd bc;
-  if (!igl::boundary_conditions(TV, TT, C, {}, BE, {}, b, bc)) {
-    return false;
-  }
-
-  igl::BBWData bbw_data;
-  bbw_data.verbosity = 2;
-  bbw_data.active_set_params.max_iter = 1000;
-  if (!igl::bbw(TV, TT, b, bc, bbw_data, W)) {
-    return false;
-  }
-  igl::normalize_row_sums(W, W);
-  W.conservativeResize(V.rows(), W.cols());
-  return true;
-}
-
 int main(int argc, char* argv[]) {
   cxxopts::Options options("skinning", "Skinning on deformed mesh");
   auto options_adder = options.add_options();
@@ -134,6 +50,8 @@ int main(int argc, char* argv[]) {
   options_adder("t,texture", "Texture path (.png)",
                 cxxopts::value<std::string>());
   options_adder("s,skeleton", "Skeleton path (.tgf)",
+                cxxopts::value<std::string>());
+  options_adder("w,weight", "Weight path (.dmat)",
                 cxxopts::value<std::string>());
   options_adder("d,deform", "Deform skeleton path (.tgf)",
                 cxxopts::value<std::string>());
@@ -167,12 +85,8 @@ int main(int argc, char* argv[]) {
   Eigen::MatrixXi BED;
   igl::readTGF(args["deform"].as<std::string>(), CD, BED);
 
-  Eigen::MatrixXd TV;
-  Eigen::MatrixXi TT, TF;
-  compute_tet_mesh(V, F, C, BE, TV, TT, TF);
-
   Eigen::MatrixXd W, M;
-  compute_bbw(V, TV, TT, C, BE, W);
+  igl::readDMAT(args["weight"].as<std::string>(), W);
   igl::lbs_matrix(V, W, M);
 
   Eigen::MatrixXd T;
