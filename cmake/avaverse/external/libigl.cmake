f5529fc60e4439617337619addd3ce5a7a4efa87
Vo Van Nghia <vanvnghia@gmail.com>
2022-12-06 17:52:51 +0100

    cmake: move cmake folder inside project name

--- cmake/avaverse/external/libigl.cmake
+++ cmake/avaverse/external/libigl.cmake
@@ -8,6 +8,6 @@ FetchContent_Declare(
   libigl
   GIT_REPOSITORY https://github.com/libigl/libigl.git
   GIT_TAG "${LIBIGL_GIT_TAG}"
-  PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different "${PROJECT_ROOT_DIR}/cmake/external/libigl.patch" <SOURCE_DIR> && git reset --hard "${LIBIGL_GIT_TAG}" && git apply <SOURCE_DIR>/libigl.patch
+  PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different "${PROJECT_ROOT_DIR}/cmake/avaverse/external/libigl.patch" <SOURCE_DIR> && git reset --hard "${LIBIGL_GIT_TAG}" && git apply <SOURCE_DIR>/libigl.patch
 )
 FetchContent_MakeAvailable(libigl)
